f015442b02209d98dd15d50f90c7da1f4aa87334
CaiJingLong <cjl_spy@163.com>
2023-04-19 08:29:55 +0800

    :sparkles: Init project

---
+++ bin/mobipkg_tools.dart
@@ -0,0 +1,5 @@
+import 'package:mobipkg_tools/mobipkg_tools.dart' as mobipkg_tools;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mobipkg_tools.calculate()}!');
+}
---
+++ test/mobipkg_tools_test.dart
@@ -0,0 +1,8 @@
+import 'package:mobipkg_tools/mobipkg_tools.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
